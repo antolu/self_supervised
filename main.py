f775fb49822b663972220bac4f21cdca089471b3
Anton Lu <anton96lu@gmail.com>
2022-05-18 15:39:25 +0200

    add argument parser to vicreg

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
+from argparse import ArgumentParser, Namespace
+
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 from moco import SelfSupervisedMethod
 from model_params import VICRegParams
 
 os.environ["DATA_PATH"] = "/mnt/data/Documents/SBB/runs/20220427_full_dataset/dataset/split_arch.csv"
 
 
-def main():
+def main(args: Namespace):
+    os.environ['DATA_PATH'] = args.dataset
+
     params = VICRegParams(
         dataset_name="aisi",
         encoder_arch='resnet101',
@@ -24,10 +30,26 @@ def main():
         lars_eta=0.02
     )
     model = SelfSupervisedMethod(params)
-    trainer = pl.Trainer(gpus=1, max_epochs=50)
+
+    checkpoint_callback = ModelCheckpoint(
+        'checkpoints',
+        'model-{epoch:02d}-{validation_loss:.2f}',
+        monitor='validation_loss',
+        save_top_k=5)
+
+    trainer = pl.Trainer(gpus=1, max_epochs=300,
+                         callbacks=[checkpoint_callback])
     trainer.fit(model)
     trainer.save_checkpoint("vicreg.ckpt")
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--dataset', help='Path to dataset .csv',
+                        required=True)
+    parser.add_argument('-c', '--checkpoint-dir', dest='checkpoint_dir',
+                        help='Path to checkpoint dir.', default='checkpoints')
+
+    args = parser.parse_args()
+
+    main(args)
